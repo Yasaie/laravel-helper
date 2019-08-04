8dbb2d302bb364381ffcf48852b61505178f8b32
Payam Yasaie <payam.yasaie@outlook.com>
2019-08-04 10:27:01 +0430

    flatten updated

--- src/Y.php
+++ src/Y.php
@@ -7,7 +7,6 @@
 
 namespace Yasaie\Helper;
 
-
 class Y
 {
     /**
@@ -16,54 +15,31 @@ class Y
      *
      * @param $object
      * @param $dots
-     * @param bool $html
      *
-     * @return array|string
+     * @return mixed
      */
-    public static function dotObject($object, $dots, $html = false)
+    public static function dotObject($object, $dots)
     {
         # extract given dotted string to array
         $extract = explode('.', $dots);
 
-        try {
-            # check if current is function
-            if (strpos($extract[0], '()') and !is_array($object)) {
-                $extract[0] = str_replace('()', '', $extract[0]);
-                $item = $object->{$extract[0]}();
-
-                # check if current index is array
-            } elseif (isset($object[$extract[0]])) {
-                $item = $object[$extract[0]];
-
-                # check if current index is object
-            } else {
-                $item = $object->{$extract[0]};
-            }
-        } catch(\Exception $e) {
-            try {
-                # check if current index is nested array/object
-                foreach ($object as $ob) {
-                    $item[] = self::dotObject($ob, $extract[0], $html);
-                }
-            } catch (\Exception $e) {
-                # finaly return null if nothing works
-                $item = null;
-            }
+        # check if current is function
+        if (strpos($extract[0], '()')) {
+            $extract[0] = str_replace('()', '', $extract[0]);
+            $item = $object->{$extract[0]}();
+        } else {
+            # check if current index is array or object
+            $item = isset($object[$extract[0]])
+                ? $object[$extract[0]] : $object->{$extract[0]};
         }
+
         # check if still has child
         if (count($extract) > 1) {
             # remove first index of object for pass to function again
             $slice = implode('.', array_slice($extract, 1));
-            return self::dotObject($item, $slice, $html);
+            return self::dotObject($item, $slice);
         }
-        # Check if it's not set return null
-        if (!isset($item)) {
-            $item = null;
-        }
-        # convert array to html if flag is true
-        if ($html and is_array($item)) {
-            $item = implode('<br>' . PHP_EOL, $item);
-        }
+
         # finaly return last child
         return $item;
     }
@@ -75,11 +51,10 @@ class Y
      * @param $items
      * @param $names
      * @param $search
-     * @param null $column
      *
-     * @return \Illuminate\Support\Collection
+     * @return mixed
      */
-    public static function flattenItems($items, $names, $search, $column = null)
+    public static function flattenItems($items, $names, $search)
     {
         $output = [];
         foreach ($items as $item) {
@@ -90,22 +65,17 @@ class Y
                 isset($name['get'])
                 or $name['get'] = $name['name'];
 
-                # check if return is string
-                $return_string = isset($name['string']) and $name['string'];
                 # get item value recurusive
-                $value = self::dotObject($item, $name['get'], $return_string);
+                $value = self::dotObject($item, $name['get']);
                 $output[$item->id]->{$name['name']} = $value;
 
                 # check if current item is searchable and change
                 # found flag if search string found in item
-                if (!$column and isset($name['visible']) and $name['visible']) {
-                    if (preg_match("/$search/i", $value))
-                        $found = true;
-
-                    # check if column is set search only there
-                } elseif ($column and $column == $name['name']) {
-                    if (preg_match("/$search/i", $value))
-                        $found = true;
+                if (!isset($name['hidden'])
+                    and !$name['hidden']
+                    and preg_match("/$search/i", $value)
+                ) {
+                    $found = true;
                 }
             }
 
@@ -113,77 +83,32 @@ class Y
             if (! $found) unset($output[$item->id]);
         }
 
-        return collect($output);
+        if (class_exists('Illuminate\Support\Collection'))
+            return collect($output);
+        else
+            return $output;
     }
 
     /**
-     * @package addAndRemove
+     * @package paginate
      * @author  Payam Yasaie <payam@yasaie.ir>
      *
-     * @param $object
-     * @param $field
-     * @param $new
+     * @param $items
+     * @param $current
+     * @param $perPage
+     *
+     * @return \stdClass
      */
-    static public function addAndRemove($object, $field, $new)
+    public static function paginate(&$items, $current, $perPage)
     {
-        $old = $object->pluck($field)->toArray();
-        $new = $new ?: [];
+        $items = $items instanceof \Illuminate\Support\Collection ? $items : collect($items);
+        $page = new \stdClass();
+        $page->current = $current;
+        $page->perPage = $perPage;
+        $page->items_count = count($items);
+        $page->count = (int)ceil($page->items_count / $page->perPage);
 
-        $added = array_diff($new, $old);
-        $removed = array_diff($old, $new);
-
-        foreach ($added as $add) {
-            $object->create([
-                $field => $add
-            ]);
-        }
-
-        $object->whereIn($field, $removed)->delete();
-    }
-
-    /**
-     * @package buildTree
-     * @author  Payam Yasaie <payam@yasaie.ir>
-     *
-     * @param $elements
-     * @param int $parentId
-     *
-     * @return array
-     */
-    public static function buildTree($elements, $parentId = 0)
-    {
-        $branch = array();
-
-        foreach ($elements as $element) {
-            if ($element->parent_id == $parentId) {
-                $children = self::buildTree($elements, $element->id);
-                if ($children) {
-                    $element->children = $children;
-                }
-                $branch[] = $element;
-            }
-        }
-
-        return $branch;
-    }
-
-    /**
-     * @package makeRoute
-     * @author  Payam Yasaie <payam@yasaie.ir>
-     *
-     * @param $query
-     * @param string $text
-     * @param null $route
-     *
-     * @return string
-     */
-    public static function makeRoute($query, $text = '', $route = null)
-    {
-        if (isset($query['route'])) {
-            $route = $query['route'];
-            unset($query['route']);
-        }
-        $query = http_build_query($query);
-        return "<a href='" . route($route) . "/?$query'>$text</a>";
+        $items = $items->forPage($page->current, $page->perPage);
+        return $page;
     }
 }
